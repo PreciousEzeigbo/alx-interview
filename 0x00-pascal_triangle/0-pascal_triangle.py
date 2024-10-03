ed95fd5aca3e65fe0e2507b1e17b5a5c020e6ebf
Precious Ezeigbo <preciousezeigbo81@gmail.com>
2024-10-03 21:35:17 +0100

    making executable and updating readme

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -21,10 +21,15 @@ def pascal_triangle(n):
     for row_num in range(1, n):
         # Start the new row with [1]
         row = [1]
-        
-        # Populate the current row with sums of adjacent numbers from the previous row
+
+        """
+        Populate the current row
+        with sums of adjacent numbers from the previous row
+        """
         for col in range(1, len(triangle[row_num - 1])):
-            row.append(triangle[row_num - 1][col - 1] + triangle[row_num - 1][col])
+            row.append(
+                triangle[row_num - 1][col - 1] + triangle[row_num - 1][col]
+            )
 
         # End the row with [1]
         row.append(1)
